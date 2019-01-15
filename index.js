a83ceb96c992c7af9465aac25b2112cc9e92ee3a
David McGrann <dpmcgrann@gmail.com>
2019-01-15 19:09:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const driver = {key: 'value'}:
+  
+  const newDriver = 
+}
